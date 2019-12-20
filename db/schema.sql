5747a7a53b8c32642de8e7a88847fd4deab6b2a7
Lazaro <Lazaro.Perez0823@gmail.com>
2019-12-19 21:26:14 -0500

    updated SQL

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,23 @@
 Drop database if exists loaned_db;
-
 CREATE DATABASE loaned_db;
 USE loaned_db;
 
-CREATE TABLE loans
+CREATE TABLE People (
+	id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(255) NOT NULL,
+	Phone_Number INTEGER NOT NULL,
+	Email VARCHAR(255) NOT NULL,
+	Photo_url VARCHAR (255),
+	PRIMARY KEY (id)
+);
+
+
+CREATE TABLE Items
 (
 	id int NOT NULL AUTO_INCREMENT,
-	burger_name varchar(255) NOT NULL,
-	devoured boolean DEFAULT false,
-	PRIMARY KEY (id)
+	PRIMARY KEY (id),
+	item_name varchar(255) NOT NULL,
+	Category varchar(255) NOT NULL,
+	Value INTEGER NOT NULL,
+	image_url VARCHAR(255)
 );
